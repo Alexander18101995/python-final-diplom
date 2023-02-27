4462415c44e9a9d75ec4e6ea88c3de03166bf08c
Alex <kudinov-ao@yandex.ru>
2023-02-27 13:53:59 +0300

    Добавлены Сигналы и конфиг email

--- orders/backend/apps.py
+++ orders/backend/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
-from backend.signals import create_profile
+from backend.signals import password_reset_token_created,new_user_registered_signal,new_order_signal
 
 class BackendConfig(AppConfig):
     name = 'backend'
 
     def ready(self):
-        create_profile
-
+        password_reset_token_created
+        new_user_registered_signal
+        new_order_signal
 
